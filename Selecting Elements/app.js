08109d50b020cd5365d9a55a5c1384d10b0da967
Nadir Chi <52305315+nadzchi@users.noreply.github.com>
2022-11-12 13:41:15 -0600

    Added listITems

--- Selecting Elements/app.js
+++ Selecting Elements/app.js
@@ -6,5 +6,7 @@ const title = document.getElementById("main-heading");
 console.log(title);
 // GetElementByClassName()
 
+const listItems = getElementsByClassName("list-item");
+console.log(listItems);
 
 // GetElementsByTagName()
